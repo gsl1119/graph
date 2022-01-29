f7a138c9eec87e4df790b5407133104384e29469
李林 <l15169449063@163.com>
2022-01-29 12:28:48 +0800

    初始化

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 set -x
 set -e
 
-id = docker ps | awk 'NR==2' | awk '{print $1}'
-docker stop $id
+docker stop $(docker ps -a -q)
 docker rmi -f  graph/springboot:1.0.0
 docker build -f dockerfile  -t  graph/springboot:1.0.0 .
 docker run -d -p 8888:8888 graph/springboot:1.0.0
